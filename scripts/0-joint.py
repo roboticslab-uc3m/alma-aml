b761bcf3ccacf90c0f234cea17ac9ffe252218ac
jgvictores <jcgvicto@gmail.com>
2023-09-12 09:55:51 +0200

    renames, init preprog

--- scripts/0-joint.py
+++ scripts/0-joint.py
@@ -1,4 +1,4 @@
-import yarp
+from time import sleep
 
 DEFAULT_HEAD_PAN = -45.0
 DEFAULT_HEAD_TILT = 0.0
@@ -6,45 +6,93 @@ DEFAULT_HEAD_TILT = 0.0
 DEFAULT_TRUNK_PAN = 45.0
 DEFAULT_TRUNK_TILT = 30.0
 
+# YARP
+import yarp
 yarp.Network.init()
 if not yarp.Network.checkNetwork():
     print('[error] Please try running yarp server')
     quit()
 
-headOptions = yarp.Property()
-headOptions.put('device','remote_controlboard')
-headOptions.put('remote','/teoSim/head')
-headOptions.put('local','/alma/teoSim/head')
-headDd = yarp.PolyDriver(headOptions)
-if not headDd.isValid():
+#-- Head (H)
+optionsH = yarp.Property()
+optionsH.put('device','remote_controlboard')
+optionsH.put('remote','/teoSim/head')
+optionsH.put('local','/alma/teoSim/head')
+ddH = yarp.PolyDriver(optionsH)
+if not ddH.isValid():
     print('[error] Cannot connect to: /teoSim/head')
     quit()
-headPos = headDd.viewIPositionControl()
+posH = ddH.viewIPositionControl()
 
-trunkOptions = yarp.Property()
-trunkOptions.put('device','remote_controlboard')
-trunkOptions.put('remote','/teoSim/trunk')
-trunkOptions.put('local','/alma/teoSim/trunk')
-trunkDd = yarp.PolyDriver(trunkOptions)
-if not trunkDd.isValid():
+#-- Trunk (T)
+optionsT = yarp.Property()
+optionsT.put('device','remote_controlboard')
+optionsT.put('remote','/teoSim/trunk')
+optionsT.put('local','/alma/teoSim/trunk')
+ddT = yarp.PolyDriver(optionsT)
+if not ddT.isValid():
     print('[error] Cannot connect to: /teoSim/trunk')
     quit()
-trunkPos = trunkDd.viewIPositionControl()
+posT = ddT.viewIPositionControl()
 
-rightArmOptions = yarp.Property()
-rightArmOptions.put('device','remote_controlboard')
-rightArmOptions.put('remote','/teoSim/rightArm')
-rightArmOptions.put('local','/alma/teoSim/rightArm')
-rightArmDd = yarp.PolyDriver(rightArmOptions)
-if not rightArmDd.isValid():
+#-- Right Arm (RA)
+optionsRA = yarp.Property()
+optionsRA.put('device','remote_controlboard')
+optionsRA.put('remote','/teoSim/rightArm')
+optionsRA.put('local','/alma/teoSim/rightArm')
+ddRA = yarp.PolyDriver(optionsRA)
+if not ddRA.isValid():
     print('[error] Cannot connect to: /teoSim/rightArm')
     quit()
-rightArmPos = rightArmDd.viewIPositionControl()
+posRA = ddRA.viewIPositionControl()
+axesRA = posRA.getAxes()
 
-trunkPos.positionMove(0, DEFAULT_TRUNK_PAN)
-trunkPos.positionMove(1, DEFAULT_TRUNK_TILT)
+posT.positionMove(0, DEFAULT_TRUNK_PAN)
+posT.positionMove(1, DEFAULT_TRUNK_TILT)
 
-headPos.positionMove(0, DEFAULT_HEAD_PAN)
-headPos.positionMove(1, DEFAULT_HEAD_TILT)
+posH.positionMove(0, DEFAULT_HEAD_PAN)
+posH.positionMove(1, DEFAULT_HEAD_TILT)
 
-rightArmPos.positionMove(3, 90) # elbow
+for i in range(axesRA):
+    posRA.setRefSpeed(i, 35)
+
+sleep(0.1)
+q = yarp.DVector(axesRA,0.0)
+posRA.positionMove(q)
+while not posRA.checkMotionDone():
+    sleep(0.1)
+
+q = yarp.DVector(axesRA,0.0)
+q[0] = -40
+q[1] = -30
+q[3] = 30
+posRA.positionMove(q)
+while not posRA.checkMotionDone():
+    sleep(0.1)
+
+q = yarp.DVector(axesRA,0.0)
+q[0] = -40
+q[1] = -70
+q[3] = 30
+posRA.positionMove(q)
+while not posRA.checkMotionDone():
+    sleep(0.1)
+
+q = yarp.DVector(axesRA,0.0)
+q[0] = 30
+q[1] = -70
+q[3] = 30
+posRA.positionMove(q)
+while not posRA.checkMotionDone():
+    sleep(0.1)
+
+q = yarp.DVector(axesRA,0.0)
+q[0] = 29.876976
+q[1] = -37.240784
+q[2] = 7.149385
+q[3] = 61.159927
+q[4] = 51.669594
+q[5] = -48.752197
+posRA.positionMove(q)
+while not posRA.checkMotionDone():
+    sleep(0.1)
