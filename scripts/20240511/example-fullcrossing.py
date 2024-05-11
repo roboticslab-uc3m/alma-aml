1d3c8382f4b9775427a5cccdcac8f94b218f3d54
jgvictores <jcgvicto@gmail.com>
2024-05-11 11:37:06 +0200

    go

--- scripts/20240511/example-fullcrossing.py
+++ scripts/20240511/example-fullcrossing.py
@@ -1,4 +1,16 @@
 import aml
 from aml import amlSimpleLibrary as sc
 
+set_of_all_constants = {0, 1, 2, ..., 7} 
+
 alg = sc.embedder()
+alg.verbose = True
+alg.binary = True # only for binary classification
+alg.calculateRedundacy = True
+alg.removeRepetitions = True
+
+alg.atomization = []
+for i in set_of_all_constants:
+    c = alg.cmanager.setNewConstantIndex()
+    at = sc.atom(alg.epoch, alg.generation, [c])
+    alg.atomization.append(at)
