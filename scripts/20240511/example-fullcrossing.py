174508ba86bc7773eed4162b8f6f51d0b712afe0
jgvictores <jcgvicto@gmail.com>
2024-05-11 12:35:16 +0200

    but works

--- scripts/20240511/example-fullcrossing.py
+++ scripts/20240511/example-fullcrossing.py
@@ -1,7 +1,8 @@
 import aml
 from aml import amlSimpleLibrary as sc
 
-set_of_all_constants = {0, 1, 2, 3, 4, 5, 6, 7} 
+#set_of_all_constants = {0, 1, 2, 3, 4, 5, 6, 7} # (b[0][0], b[0][1], b[1][0], b[1][1], w[0][0], ... )
+set_of_all_constants = {0, 1, 2, ..., 7}
 
 alg = sc.embedder()
 alg.verbose = True
@@ -20,27 +21,14 @@ vTerm = sc.LCSegment([vIndex], alg.cmanager)
 at = sc.atom(alg.epoch, alg.generation, [vIndex])
 alg.atomization.append(at)
 
-example_pos_1 = {0, 2, 5, 7}
-example_pos_2 = {0, 2, 3, 5}
+example_pos_1 = {0, 2, 5, 7} # (b,w,b,w)
+example_pos_2 = {0, 2, 3, 5} # (b,w,b,b)
 
 example_list = [example_pos_1, example_pos_2]
 
 for example in example_list:
+    print("example", example)
     exampleTerm = sc.LCSegment(example, alg.cmanager) # Here you add your examples
     region = 1
     pRel = sc.relation(vTerm, exampleTerm, True, alg.generation, region)
     alg.enforce(pRel.L, pRel.H) # full crossing
-
-
-"""
-<Calculating full crossing Traceback (most recent call last):
-  File "/home/yo/repos/jgvictores/alma-top-secret/scripts/20240511/example-fullcrossing.py", line 32, in <module>
-    alg.enforce(pRel.L, pRel.H) # full crossing
-  File "amlSimpleLibrary.py", line 2107, in amlSimpleLibrary.embedder.enforce
-  File "amlSimpleLibrary.py", line 899, in amlSimpleLibrary.cross
-  File "amlSimpleLibrary.py", line 1161, in amlSimpleLibrary.removeRedundantAtoms
-  File "amlSimpleLibrary.py", line 1117, in amlSimpleLibrary.removeRedundantAtoms_I
-  File "amlSimpleLibrary.py", line 1099, in amlSimpleLibrary.removeRedundantAtoms_L
-  File "amlSimpleLibrary.py", line 587, in amlSimpleLibrary.UCSegment_impl_wChains.__isub__
-AssertionError
-"""
