7b83c2c72f5dcc2adc2033a50ef8aa320cd945c3
jgvictores <jcgvicto@gmail.com>
2024-05-12 11:28:56 +0200

    works

--- scripts/20240511/example-fullcrossing-basic.py
+++ scripts/20240511/example-fullcrossing-basic.py
@@ -21,24 +21,11 @@ def PosRel(L, R):
     rTerm = sc.LCSegment(alg.cmanager.definedWithName[R], alg.cmanager)
     return sc.relation(lTerm, rTerm, True, alg.generation, region=1)
 
+print("* pRel1")
 pRel1 = PosRel("a","b")
 alg.enforce(pRel1.L, pRel1.H) # full crossing
-
 """
-
-term1 = sc.LCSegment({0}, alg.cmanager)
-pRel1 = sc.relation(term1, exampleTerm, True, alg.generation, region)
-alg.enforce(pRel.L, pRel.H) # full crossing
-
-example_pos_1 = {0, 2, 5, 7} # (b,w,b,w)
-example_pos_2 = {0, 2, 3, 5} # (b,w,b,b)
-
-example_list = [example_pos_1, example_pos_2]
-
-for example in example_list:
-    print("example", example)
-    exampleTerm = sc.LCSegment(example, alg.cmanager) # Here you add your examples
-    region = 1
-    pRel = sc.relation(vTerm, exampleTerm, True, alg.generation, region)
-    alg.enforce(pRel.L, pRel.H) # full crossing
+print("* pRel2")
+pRel2 = PosRel("b","c")
+alg.enforce(pRel2.L, pRel2.H) # full crossing
 """
